84a7240aff1bde52cf40f32af0ea834045266a04
Jose Miguel <josemiguelperezalbero@gmail.com>
2023-08-03 13:40:50 +0200

    commit4

--- test/formulario.spect.js
+++ test/formulario.spect.js
@@ -10,19 +10,9 @@ test.describe('Test form', () => {
 
     test('debería mostrar mensaje correcto', async ({ page }) => {
         await page.getByLabel('Nombre').fill('Ana');
-    });
-
-    test('debería mostrar mensaje correcto', async ({ page }) => {
         await page.getByLabel('Apellidos').fill('Garcia');
-    });
-
-    test('debería mostrar mensaje correcto', async ({ page }) => {
         await page.getByLabel('Email').fill('jose@gmail.com');
-    });
-    test('debería mostrar mensaje correcto', async ({ page }) => {
         await page.getByLabel('Contraseña').fill('123456');
-    });
-    test('debería mostrar mensaje correcto', async ({ page }) => {
         await page.getByLabel('Contraseña').fill('123456');
     });
 
